0036d42e764e9c8166c293e84f7761d11aac33b7
Tofikbe <karategamingyt@gmail.com>
2025-07-30 10:13:24 +0530

    Update and rename route.js to .js

--- pages/api/auth/[...nextauth]/.js
+++ pages/api/auth/[...nextauth]/.js
@@ -1,14 +1,12 @@
 import NextAuth from "next-auth"
 import TwitterProvider from "next-auth/providers/twitter"
 
-const handler = NextAuth({
+export default NextAuth({
   providers: [
     TwitterProvider({
-      clientId: "QPNDiU8D1yKcS1IAx5ojp0YDF", 
+      clientId: "QPNDiU8D1yKcS1IAx5ojp0YDF",
       clientSecret: "Mi2Sb8ChqYnC2TN7rQYhDcRYfVSF48Nc25V7bnNgkdtAs",
     }),
   ],
-  secret: "PulseAI_SECRET_KEY_123", 
+  secret: "PulseAI_SECRET_KEY_123",
 })
-
-export { handler as GET, handler as POST }
